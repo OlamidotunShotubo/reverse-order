c60de576cdaa1e6ddc1f0cd7f5b0dc6abdefe595
Dotun <pilotdot2020@gmail.com>
2022-10-06 17:10:59 +0100

    new

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter the string : ");
+string input = "";
+string reverse = "";
+input = Console.ReadLine();
+for (int i = input.Length - 1; i >= 0; i--)
+{
+    reverse += input[i];
+}
+Console.WriteLine($"The reverse string is {reverse}");
+Console.ReadLine();
+Console.Read();
